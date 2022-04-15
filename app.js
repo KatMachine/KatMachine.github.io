1729746537fb317dbf8ecf37d98bf7dc74d1b61e
KatMachine <103765959+KatMachine@users.noreply.github.com>
2022-04-15 15:34:03 -0500

    update

---
+++ app.js
@@ -0,0 +1,33 @@
+
+const url = 'https://script.google.com/macros/s/AKfycbzqeFTxioYBW9YV5IbeNk0iDfh-DwibqPtV-SqemUe-fm5srhRGJrveNKsCScGFKxEO/exec';
+const output = document.querySelector('.output');
+const output1=document.createElement('div');
+
+loadData();
+window.setInterval(loadData,10000);
+output.append(output1);
+
+function loadData()
+    {
+    console.log('ready');
+    fetch(url).then(rep => rep.json())
+    .then((data)=>
+        {
+            output1.innerHTML = '';
+            makeOutput(data);
+        })
+    }
+       
+function makeOutput(res)
+    {
+        console.log(res);
+        res.data.forEach((row) =>
+            {
+                console.log(row);
+                const table = document.createElement('div');
+                output1.append(table);
+                table.innerHTML=`${row[0]}  ${row[1]}  ${row[2]} <br>`;
+                table.classList.add('box');
+            })
+
+    }                                                          
